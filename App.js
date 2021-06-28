9c0aa335b252676a33344bbaae5bad5c1ef1c202
Anthony Perrier <perrier_anthony@live.fr>
2021-06-28 12:49:43 +0200

    Import de la police de l'app, et affichage du premier écran

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
+
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Test</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    'Jomhuria' : require('./assets/fonts/Jomhuria-Regular.ttf')
+  })
+
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={require('./assets/backgrounds/bg-home-shadowed.png')} style={styles.background}>
+          <View style={styles.homeContainer}>
+            <Text style={styles.text}>Nombre de joueurs (1 à 4)</Text>
+            <TextInput keyboardType="numeric" style={styles.input} />
+            <Pressable>
+              <Text style={styles.textBtn}>Commencer</Text>
+            </Pressable>
+          </View>
+          <StatusBar style="auto" />
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +38,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    width: '100%'
+  },
+  homeContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'white',
+    fontSize: 48,
+    fontFamily: 'Jomhuria'
+  },
+  textBtn: {
+    color: '#F2A102',
+    fontSize: 48,
+    fontFamily: 'Jomhuria',
+    marginTop: -10
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '10%',
+    fontSize: 18,
+    height: 36,
+    opacity: .8
+  }
 });
