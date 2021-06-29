a5dc64d50c189b49223ead7087e80bd3658fcd1f
Anthony Perrier <perrier_anthony@live.fr>
2021-06-29 20:35:17 +0200

    Passage sur tabbar et affichage en fonction du nombre de joueurs

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,35 +1,9 @@
 import React from 'react';
-import { ImageBackground, View } from 'react-native';
+import { ImageBackground, View, Image } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import GameTable from './GameTable';
 import { NavigationContainer } from '@react-navigation/native';
-
-const Stack = createStackNavigator()
-
-const Game = ({ players, rounds }) => {
-  const nbTables = (players.length - 2) > 0 ? 2 : 1
-  //console.log(players[0], players[1], players[2], players[3])
-
-  return (
-  <View style={styles.gameContainer}>
-    <ImageBackground source={require('../assets/backgrounds/bg-empty-shadowed.jpg')} style={styles.background}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Table1">
-          <Stack.Screen name="Table1" component={GameTable} initialParams={ { player1: players[0], player2: players[1], twoTables: nbTables > 1 } } />
-          <Stack.Screen name="Table2" component={GameTable} initialParams={ { player1: players[2], player2: players[3] } } />
-        </Stack.Navigator>
-      </NavigationContainer>
-
-      {/* <GameTable player1={players[0]} player2={players[1]} />
-      {
-        nbTables === 2 &&
-        <GameTable player1={players[2]} player2={players[4]} />
-      } */}
-    </ImageBackground>
-  </View>
-  );
-}
-
 
 
 const styles = {
@@ -40,8 +14,66 @@ const styles = {
     flex: 1,
     resizeMode: 'cover',
     width: '100%'
+  },
+  tabIcon: {
+    width: 20,
+    height: 15,
+    marginBottom: -4
   }
 }
+
+const Tabs = createBottomTabNavigator()
+
+const Game = ({ players, rounds }) => {
+  const nbTables = (players.length - 2) > 0 ? 2 : 1
+
+  function getTabBarOptions () {
+    let options
+    if (nbTables > 1) {
+      options = { 
+        showIcon: true,
+        activeBackgroundColor: '#113650', 
+        inactiveBackgroundColor: '#021422',
+        style: {height: '10%', borderTopWidth: 0}
+      }
+    } else {
+      options = {
+        showIcon: false,
+        showLabel: false,
+        style: {height: 0, borderTopWidth: 0}
+      }
+    }
+    return options
+  }
+
+  function getScreenOptions () {
+    if (nbTables > 1) {
+      return () => ({
+        tabBarIcon: () => <Image source={require('../assets/table.png')} style={styles.tabIcon}/>
+      })
+    }
+    return {}
+  }
+
+  return (
+  <View style={styles.gameContainer}>
+      <NavigationContainer>
+        <Tabs.Navigator
+          style={styles.tabbar} 
+          initialRouteName="Table 1" 
+          tabBarOptions={ getTabBarOptions() }
+          screenOptions={ getScreenOptions() }  
+          >
+          <Tabs.Screen name="Table 1" component={GameTable} initialParams={ { player1: players[0], player2: players[1] } } />
+          { nbTables > 1 &&
+          <Tabs.Screen name="Table 2" component={GameTable} initialParams={ { player1: players[2], player2: players[3] } } />
+          }
+        </Tabs.Navigator>
+      </NavigationContainer>
+  </View>
+  );
+}
+
 
 
 export default Game;
--- components/GameTable.jsx
+++ components/GameTable.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import ImageBackground from 'react-native/Libraries/Image/ImageBackground';
+
+const styles = {
+  gameContainer: {
+    flex: 1
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    width: '100%'
+  }
+}
 
 const GameTable = ({route, navigation}) => {
   const table = route.name
-  const { player1, player2, twoTables } = route.params
+  const { player1, player2 } = route.params
+
   
   return ( 
-    <Text>Table avec {player1} et {player2}</Text>
+    <View style={styles.gameContainer}>
+      <ImageBackground source={require('../assets/backgrounds/bg-empty-shadowed.jpg')} style={styles.background}>
+        <Text>Table avec {player1} et {player2}</Text>
+      </ImageBackground>
+    </View>
   );
 }
  
